55452de74e1669e1cbacc528cb0ce340890f9143
IamFreak10 <mahfujabddulla9@gail.com>
2026-01-26 17:42:51 +0600

    first commit

---
+++ src/string/code.ts
@@ -0,0 +1,24 @@
+type Obj = {
+  [key: string]: number;
+};
+function wordCount(str: string): Obj {
+  let obj: Obj = {};
+  let words: string[] = str.split(' ');
+  for (let w of words) {
+    obj[w] = (obj[w] || 0) + 1;
+  }
+  return obj;
+}
+// console.log(wordCount('Java Script Mahfuj bhai'));
+function longestWord(str: string): string {
+  let word: string = str
+    .split(' ')
+    .reduce((cur, acc) => (acc.length > cur.length ? acc : cur), '');
+
+  return word;
+}
+// console.log(longestWord('Java Script MahfujBhhai'));
+function removeDuplictae(str: string): string {
+  return [...new Set<string>(str)].join('');
+}
+console.log(longestWord('Bananana Bananana '));
