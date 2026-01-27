c9a7deadadb9ed71375c20ccaf1deb2b520b370a
IamFreak10 <user.email>
2026-01-27 21:26:13 +0600

    Leet Code Longest Common Prefix

--- src/array/code.ts
+++ src/array/code.ts
@@ -1,3 +1,5 @@
+//! RUN: node ./src/array/code.ts
+
 // LeetCode Two Sum
 function twoSum(nums: number[], target: number): number[] {
   let map = new Map<number, number>();
@@ -12,4 +14,25 @@ function twoSum(nums: number[], target: number): number[] {
 }
 let nums = [2, 7, 11, 15],
   target = 9;
-console.log(twoSum(nums, target));
+// console.log(twoSum(nums, target));
+// Longest common prefix
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) return '';
+  strs.sort();
+  let prefix = '';
+  const first= strs[0];
+  const last = strs[strs.length - 1];
+  const n = Math.min(first!.length, last!.length);
+
+  for (let i = 0; i < n; i++) {
+    
+    if (first[i] === last[i]) {
+      prefix += first[i];
+    } else {
+      break;
+    }
+  }
+  return prefix;
+}
+
+
