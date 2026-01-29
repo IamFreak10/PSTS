c7756bd15ed304d95f7bf23ac291107412fa0876
Mahfuj Abdullah <mahfujabdulla9@gmail.com>
2026-01-29 22:00:49 +0000

    Kichu code baki

--- src/array/code.ts
+++ src/array/code.ts
@@ -56,3 +56,25 @@ function intersection(nums1: number[], nums2: number[]): number[] {
   return[...inter]
 
   };  
+
+  // Find the First and the last positon of a sorted array
+// [5,7,7,8,8,10]
+function searchRange(nums: number[], target: number): number[] {
+  let low:number=0;
+  let high:number=nums.length;
+
+    
+  while(low<=high){
+    let mid=Math.floor((low+high)/2);
+    if(nums[mid]===target){
+
+      return[mid-1,mid+1];
+    }else if(nums[mid]<target){
+      low=mid+1;
+    }else{
+      high=mid-1;
+    }
+  }
+  return[-1,-1];
+
+};
