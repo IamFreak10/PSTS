d0e3344a882537b1e000cc2af9ba7f8aaf5ebb48
IamFreak10 <mahfujabdulla9@gmail.com>
2026-01-29 13:14:44 +0600

    Set Problem

--- src/array/code.ts
+++ src/array/code.ts
@@ -47,4 +47,12 @@ function removeDuplicates(nums: number[]): number {
   }
   return cnt+1;
 }
-console.log(removeDuplicates([1, 1, 2]));
+// console.log(removeDuplicates([1, 1, 2]));
+
+
+// intersection in array
+function intersection(nums1: number[], nums2: number[]): number[] {
+  let inter:Set<number>=new Set([...nums1].filter(x=>nums2.includes(x)))
+  return[...inter]
+
+  };  
