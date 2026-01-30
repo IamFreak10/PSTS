81f310426bbe1b457e6f7f3ddfbf7eac4048d926
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-30 20:53:11 +0600

    509. Fibonacci Number

--- src/DS/Recursion/Leet.ts
+++ src/DS/Recursion/Leet.ts
@@ -1,4 +1,4 @@
-// 50. Pow(x, n)
+//* 50. Pow(x, n)
 function myPow(x: number, n: number): number {
   if (n == 0) return 1;
   if (n < 0) {
@@ -11,4 +11,16 @@ function myPow(x: number, n: number): number {
     return x * myPow(x, n - 1);
   }
 }
-console.log(myPow(4, -2));
+// console.log(myPow(4, -2));
+
+//*  509. Fibonacci Number
+
+function fib(n: number): number {
+  let map: Map<number, number> = new Map();
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (map.has(n)) return map.get(n)!;
+  const res = fib(n - 1) + fib(n - 2);
+  map.set(n, res);
+  return res;
+}
